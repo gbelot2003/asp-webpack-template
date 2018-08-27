157e17d63a4117ba2ff008869afb0b192083a4a6
Gerardo Belot <gbelot2003@hotmail.com>
2018-08-26 23:49:43 -0600

    Add project files.

---
+++ WebApiApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiApp2.Startup))]
+namespace WebApiApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
